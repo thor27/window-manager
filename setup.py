4e5669a98cdf008d2e8495e327e15015c92bdebe
Eduardo Santos <eduardo@eduardosan.com>
2016-02-22 23:06:59 -0300

    Adiciona esqueleto do projeto

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'VERSION')) as f:
+    VERSION = f.read()
+
+requires = [
+    'tornado'
+]
+
+setup(
+    name='window-manager',
+    version=VERSION,
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/eduardosan/window-manager.git',
+    license='CPL v3.0',
+    author='Eduardo Santos',
+    author_email='eduardo@eduardosan.com',
+    description='Manage Gnome Windows',
+    install_requires=requires,
+)
